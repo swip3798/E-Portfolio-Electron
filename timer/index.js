3825066c6db85b62e27522a72a8e21acb0d9042d
Christian Schweigel <christian.david.schweigel@sap.com>
2019-04-29 21:33:47 +0200

    Add gitignore, change version of electron add webpreferences

--- timer/index.js
+++ timer/index.js
@@ -1,50 +1,39 @@
-
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+
 
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
-// Listen for app to be ready
 app.on('ready', function() {
-    //Create new Window
-
-    mainWindow = new BrowserWindow({});
-
-    //load html file
+    mainWindow = new BrowserWindow({webPreferences: {nodeIntegration: true}});
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
         slashes: true
     }));
-
-    // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
 });
 
-// Handle add Item
-
-function addRound() {
+function addRound(){
     mainWindow.webContents.send('round:add');
 }
 
-// Create menu template
-
 const mainMenuTemplate = [
     {
-        label: 'File',
+        label: "File",
         submenu:[
             {
-                label: 'Add Round',
+                label: "Add round",
                 click(){
                     addRound();
                 }
             },
             {
-                label: 'Quit',
+                label: "Quit",
                 accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
                 click(){
                     app.quit();
@@ -52,4 +41,4 @@ const mainMenuTemplate = [
             }
         ]
     }
-];
+]
